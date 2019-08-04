f4983ee5d2f078c13880faf69ff8b3beb17ccfb2
Nicholas Cross <nicholas@nacrossweb.com>
2019-08-04 16:17:21 +1000

    Move platforms to the correct spot

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@ import PackageDescription
 
 let package = Package(
     name: "StateTransition",
-    products: [
-        .library(name: "StateTransition", targets: ["StateTransition"]),
-        ],
-    dependencies: [],
-    targets: [
-        .target(name: "StateTransition", dependencies: [], path: "StateTransition"),
-        .testTarget(name: "StateTransitionTests", dependencies: ["StateTransition"], path: "StateTransitionTests"),
-        ],
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "StateTransition", targets: ["StateTransition"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "StateTransition", dependencies: [], path: "StateTransition"),
+        .testTarget(name: "StateTransitionTests", dependencies: ["StateTransition"], path: "StateTransitionTests"),
     ]
 )
 
